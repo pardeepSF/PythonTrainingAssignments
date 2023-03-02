b0de9092937a38edf8de24cfb14499d562dbddf0
pardeep kumar <pardeep.kumar@sourcefuse.com>
2023-03-02 16:50:27 +0530

    mistakes corrected

--- test/test.py
+++ test/test.py
@@ -10,20 +10,20 @@ class TestCalculations(unittest.TestCase):
         self.calculation = Calculations(8, 2)
 
     def test_sum(self):
-        self.assertEqual(self.calculation.get_sum(), 10, 'The sum is wrong.')
-        self.assertNotEqual(self.calculation.get_sum(), 5, 'The sum is wrong.')
+        self.assertEqual(self.calculation.get_sum(), 10, 'The sum is correct.')
+        self.assertNotEqual(self.calculation.get_sum(), 5, 'The sum is not equal to 5.')
 
     def test_diff(self):
-        self.assertEqual(self.calculation.get_difference(), 6, 'The difference is wrong.')
-        self.assertNotEqual(self.calculation.get_difference(), 2, 'The difference is wrong.')
+        self.assertEqual(self.calculation.get_difference(), 6, 'The difference is correct.')
+        self.assertNotEqual(self.calculation.get_difference(), 2, 'The difference is not equal to 2.')
 
     def test_product(self):
-        self.assertEqual(self.calculation.get_product(), 16, 'The product is wrong.')
-        self.assertNotEqual(self.calculation.get_product(), 10, 'The product is wrong.')
+        self.assertEqual(self.calculation.get_product(), 16, 'The product is correct.')
+        self.assertNotEqual(self.calculation.get_product(), 10, 'The product is not equal to 10.')
 
     def test_quotient(self):
-        self.assertEqual(self.calculation.get_quotient(), 4, 'The quotient is wrong.')
-        self.assertNotEqual(self.calculation.get_quotient(), 3, 'The division is wrong.')
+        self.assertEqual(self.calculation.get_quotient(), 4, 'The quotient is correct.')
+        self.assertNotEqual(self.calculation.get_quotient(), 3, 'The division is not equal to 3.')
         self.calc = Calculations(7,0)
         self.assertEqual(self.calc.get_quotient(), 'Can\'t divide with zero', 'The division is wrong.')
 
